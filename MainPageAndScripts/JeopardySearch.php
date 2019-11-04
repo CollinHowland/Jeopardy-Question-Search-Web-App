8f2bd81dd9ed223bc6bc423ed6b55752bfd187b6
CollinHowland <collin.howland@hotmail.com>
2019-11-03 20:43:14 -0600

    slightly better visuals

--- MainPageAndScripts/JeopardySearch.php
+++ MainPageAndScripts/JeopardySearch.php
@@ -9,31 +9,44 @@
 
 <body>
 
-<!-- <form id="search_form"> -->
-    <label>Value:</label>
-    <select name="value_of_questions" id="value_of_questions">
-        <option value="no_input" selected></option>
-        <option value="200">200</option>
-        <option value="400">400</option>
-        <option value="600">600</option>
-        <option value="800">800</option>
-        <option value="1000">1000</option>
-    </select>
-    <label>Date:</label>
-    <input type="date" name="date_of_questions" id="date_of_questions"/>
-    <select name="type_of_date" id="type_of_date" hidden>
-        <option value="day">day</option>
-        <option value="month">month</option>
-        <option value="year">year</option>
-    </select>
-    <label>Category:</label>
-    <input type="number" name="category_id" id="category_id" min="1" max="11510" step="1">
+<h1 class="main_title">Search for Jeopardy Questions!</h1>
+
+<!-- Search-related fields -->
+<div class="search_fields">
+    <div class="search_field_group">
+        <label>Value:</label>
+        <select name="value_of_questions" id="value_of_questions">
+            <option value="no_input" selected></option>
+            <option value="200">200</option>
+            <option value="400">400</option>
+            <option value="600">600</option>
+            <option value="800">800</option>
+            <option value="1000">1000</option>
+        </select>
+    </div>
+    <div class="search_field_group">
+        <label>Date:</label>
+        <input type="date" name="date_of_questions" id="date_of_questions"/>
+        <select name="type_of_date" id="type_of_date" hidden>
+            <option value="day">day</option>
+            <option value="month">month</option>
+            <option value="year">year</option>
+        </select>
+    </div>
+    <div class="search_field_group">
+        <label>Category:</label>
+        <input type="number" name="category_id" id="category_id" min="1" max="11510" step="1">
+    </div>
     <input type="submit" name="search_submit" id="search_submit" value="Search"/>
-<!-- </form> -->
+</div>
 
-<button id="search_previous_page">Previous Page</button>
-<button id="search_next_page">Next Page</button>
+<!-- Buttons to cycle between pages of loaded results-->
+<div class="next_and_previous_buttons">
+    <button id="search_previous_page">Previous Page</button>
+    <button id="search_next_page">Next Page</button>
+</div>
 
+<!--Table that will display results -->
 <div id="search_results">
     <table class="search_results_table" id="search_results_table">
         <thead>
@@ -74,9 +87,9 @@
             <label class="generic_label">Airdate: </label>
             <p class="detailed_view_data" id="detailed_view_airdate"></p><br>
         </div>
-        <!-- <div class="mpopup-foot">
-            <p>Enter new information for the event</p>
-        </div> -->
+        <div class="popup_foot">
+            <p>Select a "Search This" button to restrict the results to that category</p>
+        </div>
     </div>
 </div>
 
